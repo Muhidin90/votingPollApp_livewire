9fad5f9e4287c405c3e1afd098dc9ca543442b4a
muhidin90 <muhidinsaid97@gmail.com>
2023-10-30 09:03:16 +0300

    genesis commit

---
+++ app/Models/option.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class option extends Model
+{
+    use HasFactory;
+    public function pool():BelongsTo{
+        return $this->belongsTo(pool::class);
+    }
+
+    public function vote():HasMany 
+    {
+        return $this->hasMany(vote::class);
+    }
+}
---
+++ database/migrations/2023_10_30_054750_create_options_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\pool;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('options', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); 
+            $table->timestamps();
+            $table->foreignIdFor(pool::class)->constrained();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('options');
+    }
+};
